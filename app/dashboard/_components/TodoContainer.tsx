89ebe063a082d4b871218bc4022dcacdf13ee47b
Yukihiro1123 <murasima1123@gmail.com>
2023-12-15 23:26:43 +0900

    dndフロント完成

--- app/dashboard/_components/TodoContainer.tsx
+++ app/dashboard/_components/TodoContainer.tsx
@@ -2,23 +2,50 @@
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 import { Todo } from "@prisma/client";
 import { TodoList } from "./TodoList";
+import { useEffect, useState } from "react";
 
 interface TodoContainerProps {
   todoList: Todo[];
 }
 
-const onDragEnd = (result: any) => {
-  const { destination, source, type } = result;
-  console.log(destination, source, type);
-};
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+}
 
 export const TodoContainer = ({ todoList }: TodoContainerProps) => {
+  const [data, setData] = useState(todoList);
+  useEffect(() => {
+    setData(data);
+  }, [data]);
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+    if (!destination) {
+      return;
+    }
+    // if dropped in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    // User moves a list
+    if (type === "list") {
+      const items = reorder(data, source.index, destination.index).map(
+        (item, index) => ({ ...item, order: index })
+      );
+      setData(items);
+    }
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="lists" type="list">
         {(provided) => (
           <div {...provided.droppableProps} ref={provided.innerRef}>
-            {todoList.map((todo, index) => (
+            {data.map((todo, index) => (
               <TodoList todo={todo} key={todo.todoId} index={index} />
             ))}
             {provided.placeholder}
