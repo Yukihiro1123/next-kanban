c6cc4313c5061b107be7e4779dc33d63e7c281d1
Yukihiro1123 <murasima1123@gmail.com>
2023-12-17 14:24:23 +0900

    navbarにリンクつける+フォント

--- app/dashboard/[boardId]/_components/_board/BoardNavbar.tsx
+++ app/dashboard/[boardId]/_components/_board/BoardNavbar.tsx
@@ -10,8 +10,12 @@ export const BoardNavbar = ({ data }: BoardNavbarProps) => {
   return (
     <div className="flex justify-between">
       <div className="pl-3">
-        <p>{data.title}</p>
-        <p>{data.description}</p>
+        <h3 className="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          {data.title}
+        </h3>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          {data.description}
+        </p>
       </div>
       <Button variant="ghost">
         <MoreHorizontalIcon />
